43168897f54c948584c05ad458142bf432ba14df
Daniyar <daniyar2109@mail.ru>
2023-08-30 17:12:00 +0600

    Initial commit

---
+++ m16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m16
+//
+//  Created by Daniyar Zhakipbayev on 30.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
